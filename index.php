6158a1734d277405ddf6c46e6dc5abf18fbd76b8
Antonello Dettori <tonello96'gmail.com>
2015-11-30 19:14:03 +0000

    Added some more php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>TBD</title>
+		<title>Google I/O 2015</title>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
@@ -26,42 +26,7 @@
 	
 	<body>
         <div class="h-container">	
-		    <header>
-			    <img src="images/banner-google.jpg" class="img-responsive">
-
-		    </header>
-		
-		
-		    <nav class="navbar navbar-default">
-  			    <div class="container-fluid">
-    				    <div class="navbar-header">
-						    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-						        <span class="icon-bar"></span>
-							    <span class="icon-bar"></span>
-        					    <span class="icon-bar"></span>                        
-      					    </button>
-      					    <a class="navbar-brand" href="#">
-                                <img src="images/navbar-logo.png" class="icon-img">
-                            </a>
-    				    </div>
-
-    				    <div class="collapse navbar-collapse" id="myNavbar">
-      					    <ul class="nav navbar-nav">
-        					    <li class="active"><a href="#">Home</a></li>
-        					    <li class="dropdown">
-          						    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Event<span class="caret"></span></a>
-          						    <ul class="dropdown-menu">
-            							<li><a href="#">Page 1-1</a></li>
-            							<li><a href="#">Page 1-2</a></li>
-            							<li><a href="#">Page 1-3</a></li>
-          						    </ul>
-        					    </li>
-        					    <li><a href="#">About</a></li>
-        					    <li><a href="#">Tutor page</a></li>
-      					    </ul>
-    				    </div>
-  			    </div>
-		    </nav>
+		    <?php require 'pages/template/header.php';?>
 		
 
 		    <div class="container-fluid">
@@ -104,22 +69,7 @@
 
 			    </div>
 
-
-			    <footer class="row">
-
-				    <div class="col-sm-4">
-					    <h4>Information<h3/>
-				    </div>
-
-				    <div class="col-sm-4"> 
-					    <h4>About<h3/>
-				    </div>
-
-				    <div class="col-sm-4">
-					    <h4>Terms and conditions</h3>
-				    </div>
-
-		    	</footer>
+                <?php require 'pages/template/footer.php';?>
 		
 	    	</div>
         </div>
---
+++ pages/template/footer.php
@@ -0,0 +1,17 @@
+<?php		
+	echo '<footer class="row">
+
+                    <div class="col-sm-4">
+                        <h4>Information<h3/>
+                    </div>
+
+                    <div class="col-sm-4"> 
+                        <h4>About<h3/>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <h4>Terms and conditions</h3>
+                    </div>
+
+          </footer>
+'?>
