5370c9ea068b1e28e54d4883ebd11aee5db0748e
Antonello Dettori <tonello96'gmail.com>
2015-12-02 22:01:14 +0000

    Templated dependencies

--- index.php
+++ index.php
@@ -5,25 +5,7 @@
 
 		<title>Google I/O 2015</title>
 
-        <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
-
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-		<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-		<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
-
-		<script type="text/javascript" src="slick/slick.min.js"></script>
-		<script type="text/javascript" src="slick/slick.min.js"></script>
-
-		<script src="website-script.js"></script>
-
-		<link rel="stylesheet" type="text/css" href="website-style.css"/>
+        <?php require_once "template/dependencies.php"?>
 
         <?php require_once "template/functions.php"?>
 
