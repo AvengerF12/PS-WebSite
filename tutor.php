31ccdeaed1a68bb47033b76f07fbac2e4be947ff
Antonello Dettori <tonello96'gmail.com>
2015-12-03 15:54:13 +0000

    Added breakpoints to tutor.php

--- tutor.php
+++ tutor.php
@@ -19,10 +19,12 @@
 
                 <ul>
                     <li>A clear use of HTML5 tags including text, images and links.</li>
+                    <BR>
                     <p class="text-center">All of the html pages use the html5 tags to define the structure of the content.
                         The tags used include: article, aside, nav, section, footer, header.
                     </p>
                     <li>Use of a CSS framework, such as Bootstrap, to provide responsive layout.</li>
+                    <BR>
                     <p class="text-center">
                         The website is completely responsive through the use of Bootstrap. Its layout adapts depending on the device or the dpi.
                     </p>
@@ -31,6 +33,7 @@
                         <li>DOM element selection</li>
                         <li>An event</li>
                         <li>A DOM manipulation method</li>
+                        <BR>
                         <p class="text-center">
                             The "Load more news" button on the homepage uses javascript to accomplish all of these three requirements. 
                             It selects the next three hidden news through a DOM element selection, it waits for a onClick event to occur to the button and manipulates the DOM to show the next three hidden news.
@@ -39,6 +42,7 @@
                     </ul>
                     </li>
                     <li>Use of jQuery which demonstrates an event with an effect</li>
+                        <BR>
                         <p class="text-center">
                             I used jQuery to hide the news in excess right after the page was finished loading.
                             The .ready event is used to call the jQuery hide() effect, on all the news after the third one, right after the page is loaded.
@@ -49,6 +53,7 @@
                         <li>Reading a table in a database</li>
                         <li>or</li>
                         <li>Writing to a table in a database</li>
+                        <BR>
                         <p class="text-center">
                             Both of these requirements have been fulfilled.
                             The web application keeps track of the number of visits for each article and writes them in the database.
